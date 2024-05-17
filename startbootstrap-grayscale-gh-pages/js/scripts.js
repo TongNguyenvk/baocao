f344fc58e708c358209f4f94feb62a5136ae631a
TongNguyenvk <tongct08@gmail.com>
2024-05-17 14:20:01 +0700

    bao cao

--- startbootstrap-grayscale-gh-pages/js/scripts.js
+++ startbootstrap-grayscale-gh-pages/js/scripts.js
@@ -52,3 +52,11 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+function showPopup() {
+    document.getElementById('popup').classList.add('show');
+}
+
+function closePopup() {
+    document.getElementById('popup').classList.remove('show');
+}
